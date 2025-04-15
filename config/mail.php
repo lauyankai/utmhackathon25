b69fb6718ef8aea7f56eb6536b1224d049bfec61
lauyankai <lauyankai04@gmail.com>
2025-04-15 09:45:50 +0800

    Revert

--- config/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'smtp_host' => 'smtp.gmail.com',
-    'smtp_port' => 587,
-    'smtp_username' => 'evelynyuan@graduate.utm.my',
-    'smtp_password' => 'dulk dxys xhtv gunq',
-    'from_address' => 'evelynyuan@graduate.utm.my',
-    'from_name' => 'Koperasi KADA'
-]; 
